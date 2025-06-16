0bea17bd7a3e18e471bd8ce34efe57cd9555181d
bobwiths <marcelosachett@gmail.com>
2025-06-16 16:52:48 -0300

    added valid/invalid credentials login

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,15 +1,19 @@
+import userData from '../data/userData.json'
 import { expect } from '@wdio/globals'
 import { $ } from '@wdio/globals'
-import MenuScreen from '../screen/menu.screen'
+import MenuScreen from '../screen/menu.screen.js'
+import LoginScreen from '../screen/login.screen.js'
 
 describe('My Login application', () => {
     it('should not login with invalid credentials', async () => {
        await MenuScreen.accessLoginScreen()
-       await $("~Username input field").setValue('wrongUser')
-       await $("~Password input field").setValue('123456')
-       await $("~Login button").click()
-       await expect($('//android.view.ViewGroup[@content-desc="generic-error-message"]/android.widget.TextView')).toHaveText("Provided credentials do not match any user in this service.")
-
+       await LoginScreen.fillLoginForm(userData.userFail.username, userData.userFail.password)
+       await expect($(LoginScreen.errorMessage)).toHaveText("Provided credentials do not match any user in this service.");
+    })
+    it('should login with valid credentials', async () => {
+        await MenuScreen.accessLoginScreen()
+        await LoginScreen.fillLoginForm(userData.userSuccess.username, userData.userSuccess.password)
+        await expect($(LoginScreen.containerHeader))
     })
 })
 
